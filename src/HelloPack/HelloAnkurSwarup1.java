88520baa75825cb885b38890fcdf500f117186ce
Swarup Kshatriya <swarup.kshatriya@gmail.com>
2023-12-09 16:13:51 -0500

    Adding HelloAnkurSwarup1 and TestDataSheet.xls

--- src/HelloPack/HelloAnkurSwarup1.java
+++ src/HelloPack/HelloAnkurSwarup1.java
@@ -2,13 +2,17 @@ package HelloPack;
 
 public class HelloAnkurSwarup1 {
 
+
 	public static void main(String args[]) {
 		System.out.println("Hi this is swarup000");
 		System.out.println("Hi this is swarup001");
 		System.out.println("Hi this is swarup002");
 		System.out.println("Hi I am adding some value to it");
+		System.out.println("I am doing some practice on git hub");
+		System.out.println("this is what i Learned");
 	}
 
 
 		
+
 }
